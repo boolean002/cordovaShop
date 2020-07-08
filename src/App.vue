714b6ac09b524bdf495dee85de8fbe4577e5c3a8
bulim <jsbulim@itcom888.com>
2020-07-08 20:12:20 +0800

    优化vue底部导航

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <div id="app">
     <router-view/>
+    <navigate/>
   </div>
 </template>
 
 <script>
 import './assets/style/common.css';
-
+import navigate from './components/footer/navigate.vue'
 export default {
-  name: 'app'
+  name: 'app',
+  components:{
+        navigate
+    }
 }
 </script>
 
--- src/components/footer/navigate.vue
+++ src/components/footer/navigate.vue
@@ -1,9 +1,9 @@
 <template>
     <div style="height:50px;">
-    <van-tabbar >
-        <van-tabbar-item icon="wap-home" to="/home">首页</van-tabbar-item>
-        <van-tabbar-item icon="wap-nav" to="/category" >分类</van-tabbar-item>
-        <van-tabbar-item icon="cart" to="/cart" >购物车</van-tabbar-item>
+    <van-tabbar  active-color="#000" v-model='active'>
+        <van-tabbar-item icon="wap-home" replace  to="/home">首页</van-tabbar-item>
+        <van-tabbar-item icon="wap-nav"   to="/category" >分类</van-tabbar-item>
+        <van-tabbar-item icon="cart"  to="/cart" >购物车</van-tabbar-item>
         <van-tabbar-item icon="contact" to="/user/index">我的</van-tabbar-item>
     </van-tabbar>
     </div>
@@ -16,6 +16,28 @@ export default {
     components:{
         [Tabbar.name]: Tabbar,
         [TabbarItem.name]: TabbarItem,
+    },
+    data(){
+        return {
+            active:0
+        }
+    },
+    created(){
+        console.log("this",this.$route)
+    },
+    watch:{
+        "$route.name"(newV){
+            this.asyncActive();
+        }
+    },
+    methods:{
+        asyncActive(){
+          let path=['home','category','cart','user']
+           let index= path.indexOf(this.$route.name);
+           if(index>-1){
+               this.active=index;
+           }
+        }
     }
 }
 </script>
